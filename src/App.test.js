7029d8c4d586bc0b63b0d8fff9b11cf343059c6f
brians123 <brianssui@yahoo.com>
2020-05-16 23:13:40 -0400

    travis yaml file working

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,37 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { render } from "@testing-library/react";
+import {
+  waitFor,
+  getByLabelText,
+  getByText,
+  queryByTestId,
+  getByTestId,
+} from "@testing-library/dom";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+function getDOM() {
+  const div = document.createElement("div");
+  div.innerHTML = `
+    <label for="username">Username</label>
+    <input id="username" />
+    <button>Print Username</button>
+  `;
+  return div;
+}
+
+test("renders learn react link", async () => {
+  // const { getByText } = render(<App />);
+  // const linkElement = getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
+  const famousWomanInHistory = "Ada Lovelace";
+  const container = getDOM();
+
+  // Get form elements by their label text.
+  // An error will be thrown if one cannot be found (accessibility FTW!)
+  const input = getByLabelText(container, "Username");
+  input.value = famousWomanInHistory;
+
+  // jest snapshots work great with regular DOM nodes!
+  expect(container).toMatchSnapshot();
 });
