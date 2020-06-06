ea0b582ae1b87eea037e26dbdfa377c1d00b3cdf
brians123 <brianssui@yahoo.com>
2020-06-05 21:02:15 -0400

    testing content with cypress

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -2,4 +2,9 @@ describe("Test App", () => {
   it("launches", () => {
     cy.visit("/");
   });
+
+  it("login button shows", () => {
+    cy.visit("/");
+    cy.contains("Sign in with Google");
+  });
 });
